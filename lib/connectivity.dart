1d88f93427c4c7a55f251bdf38f483ddb1e688be
amit-sss <amit.geed@shahsoftsol.com>
2024-07-12 01:19:47 +0530

    commented connectivity plus due to deprecation issues.

--- lib/connectivity.dart
+++ lib/connectivity.dart
@@ -1,4 +1,4 @@
-import 'package:connectivity_plus/connectivity_plus.dart' as cp;
+// import 'package:connectivity_plus/connectivity_plus.dart' as cp;
 
 abstract class Connectivity {
   bool isConnected();
@@ -7,18 +7,18 @@ abstract class Connectivity {
 class InternetConnectivity extends Connectivity {
   bool _isConnected = true;
 
-  InternetConnectivity() {
-    cp.Connectivity().onConnectivityChanged.listen((result) {
-      _updateConnectivityStatus(result);
-    });
-  }
+  // InternetConnectivity() {
+  //   cp.Connectivity().onConnectivityChanged.listen((result) {
+  //     _updateConnectivityStatus(result);
+  //   });
+  // }
 
   @override
   bool isConnected() {
     return _isConnected;
   }
 
-  void _updateConnectivityStatus(cp.ConnectivityResult result) {
-    _isConnected = result != cp.ConnectivityResult.none;
-  }
+  // void _updateConnectivityStatus(cp.ConnectivityResult result) {
+  //   _isConnected = result != cp.ConnectivityResult.none;
+  // }
 }
